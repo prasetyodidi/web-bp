32b00b4f08ae61bfb6b3a5af62d52647411edd18
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-10-22 11:29:25 +0700

    update table column reference

--- database/migrations/2023_10_22_041734_create_post_tags_table.php
+++ database/migrations/2023_10_22_041734_create_post_tags_table.php
@@ -12,8 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('post_id');
+            $table->unsignedInteger('tag_id');
             $table->timestamps();
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
@@ -22,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tags');
+        Schema::dropIfExists('post_tag');
     }
 };
