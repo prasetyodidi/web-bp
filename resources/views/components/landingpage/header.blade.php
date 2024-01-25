8875214c55903f832886f88c1c0b4fb27b314a19
prasetyodidi <didiprasetyo36@gmail.com>
2024-01-25 18:45:08 +0700

    add header landing page

---
+++ resources/views/components/landingpage/header.blade.php
@@ -0,0 +1,30 @@
+<header class="z-20 fixed top-0 left-0 right-0 flex flex-row px-4 py-2 items-center justify-between bg-white">
+    <div></div>
+    <nav class="flex flex-row items-center gap-3">
+        <div class="group flex flex-col items-center">
+            <a href="#" class="text-lg text-primary-blue">
+                Home
+            </a>
+            <span class="w-0 h-1 bg-primary-blue group-hover:w-full transition ease-in-out duration-300"></span>
+        </div>
+        <div class="group flex flex-col items-center">
+            <a href="#" class="text-lg text-primary-blue">
+                Service
+            </a>
+            <span class="w-0 h-1 bg-primary-blue group-hover:w-full transition ease-in-out duration-300"></span>
+        </div>
+        <div class="group flex flex-col items-center">
+            <a href="#" class="text-lg text-primary-blue">
+                Our Works
+            </a>
+            <span class="w-0 h-1 bg-primary-blue group-hover:w-full transition ease-in-out duration-300"></span>
+        </div>
+        <div class="group flex flex-col items-center">
+            <a href="#" class="text-lg text-primary-blue">
+                About us
+            </a>
+            <span class="w-0 h-1 bg-primary-blue group-hover:w-full transition ease-in-out duration-300"></span>
+        </div>
+    </nav>
+    <x-heroicon-s-user-circle class="w-12 text-primary-blue"/>
+</header>
