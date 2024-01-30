d11f1a06a693416c4d41e5f51d68d804e0b7008e
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-01-30 16:07:31 +0700

    landing page sections

---
+++ resources/views/components/landingpage/our-works.blade.php
@@ -0,0 +1,53 @@
+<div id='our-works' class='min-h-screen flex flex-col gap-8 items-center px-24'>
+    <h1 class="text-4xl text-secondary-blue font-bold">Our Works</h1>
+    <p class="text-3xl text-slate-600 font-semibold">
+        Some of the projects we have made recently
+    </p>
+    <div class='flex flex-row justify-between gap-24'>
+        <div class='flex flex-col h-96 rounded-[1rem] shadow-2xl'>
+            <div class='relative h-1/2 flex flex-row rounded-t-[1rem] bg-primary-green'>
+                <img src="/images/ornament-3.png" alt='just an ornament' class='absolute top-0 left-0' />
+                <img src="/images/ornament-4.png" alt='just an ornament' class='absolute top-8 right-10' />
+                <div class='z-10 flex flex-col py-8 pl-8 w-2/3'>
+                    <h2 class='text-3xl text-white font-semibold'>Example App</h2>
+                    <p class='text-white'>Lorem ipsum dolor sit amet, consectetur </p>
+                </div>
+                <img src="images/work-mobile.png" alt='image of example app mobile' class='z-10' />
+            </div>
+            <div class='p-8'>
+                <h2 class='text-3xl font-semibold text-slate-900'>Mobile Apps Project</h2>
+                <p class='text-slate-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt</p>
+            </div>
+        </div>
+        <div class='flex flex-col h-96 rounded-[1rem] shadow-2xl'>
+            <div class='relative h-1/2 flex flex-row rounded-t-[1rem] bg-secondary-blue'>
+                <img src="/images/work-web-2.png" alt='just an ornament' class='absolute bottom-0 left-8' />
+                <img src="/images/work-web-1.png" alt='just an ornament' class='absolute top-0 right-8' />
+                <h2 class='absolute top-8 left-8 text-3xl text-white font-semibold'>Example</h2>
+                <h2 class='absolute bottom-8 right-10 text-3xl text-white font-semibold'>Website</h2>
+            </div>
+            <div class='p-8'>
+                <h2 class='text-3xl font-semibold text-slate-900'>Website Project</h2>
+                <p class='text-slate-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt</p>
+            </div>
+        </div>
+        <div class='flex flex-col h-96 rounded-[1rem] shadow-2xl'>
+            <div class='relative h-1/2 flex flex-row  justify-betweenrounded-t-[1rem] bg-primary-red px-4'>
+                <img src="/images/work-design.png" alt='ornament design' />
+                <div class='flex flex-col justify-between w-full items-center my-8'>
+                    <h2 class='text-3xl text-white font-semibold'>Example</h2>
+                    <h2 class='text-3xl text-white font-semibold'>Graphic</h2>
+                    <h2 class='text-3xl text-white font-semibold'>Design</h2>
+                </div>
+            </div>
+            <div class='p-8'>
+                <h2 class='text-3xl font-semibold text-slate-900'>Design Project</h2>
+                <p class='text-slate-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt</p>
+            </div>
+        </div>
+    </div>
+    <a href="#" class='text-white text-xl bg-primary-blue rounded-full px-32 py-2'>See More</a>
+</div>
